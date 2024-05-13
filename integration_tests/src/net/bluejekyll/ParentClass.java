f5a1e40c0b0180aee6f6af86ecd45970fa4eea97
Sebastian Imlay <sebastian.imlay@gmail.com>
2024-05-12 21:17:55 -0400

    Add IntoJavaValue and FromJavaToRust traits for Object

--- integration_tests/src/net/bluejekyll/ParentClass.java
+++ integration_tests/src/net/bluejekyll/ParentClass.java
@@ -4,4 +4,9 @@ public class ParentClass {
     public final int call_dad(int val) {
         return val;
     }
+
+    // This is to test the `FromJavaToRust` and `IntoJavaValue` traits for Object.
+    public final Object call_dad_object(Object val) {
+        return val;
+    }
 }
